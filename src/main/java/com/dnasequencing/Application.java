85056ca2fa3900770cf9ce182585eb58f97a7bb4
jakwinhub <jakwin@gmx.de>
2024-06-23 20:52:33 +0200

    Implement the main class 'Application', which enables the application to be executed.

--- src/main/java/com/dnasequencing/Application.java
+++ src/main/java/com/dnasequencing/Application.java
@@ -1,8 +1,23 @@
 package main.java.com.dnasequencing;
 
+import javax.swing.JFrame;
+
+import main.java.com.dnasequencing.gui.DnaAnalyzerPanel;
+
 /**
- * The main class that starts the program.
+ *  The main class that starts the program.
+ *  Contains settings for panel (size and close-Operation).
  */
 
 public class Application {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("DNA Sequencing Application");
+        DnaAnalyzerPanel panel = new DnaAnalyzerPanel();
+
+        frame.add(panel);
+        frame.setSize(800, 600);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+        frame.setVisible(true);
+    }
 }
